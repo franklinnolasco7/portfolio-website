e5b50f040ecc567d7231801d81d9dd01be4f566f
FrankIncognito <nolascofranklin09@gmail.com>
2024-06-01 07:37:22 +0800

    test if resume.pdf work

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,7 +13,7 @@
       </div>
       <p class="text-white pr-4 fade-in-from-left">Welcome to My personal website. <span class="wave">👋</span></p>
       <br>
-<a href="/assets/resume.pdf" download="resume.pdf">
+<a href="https://github.com/franklinnolasco7/portfolio-website/blob/main/public/resume.pdf" download="resume.pdf">
   <button
   class="fadein-bot fade-500 flex items-center py-2 px-4 mx-auto text-sm font-medium rounded-lg border transition duration-300 md:py-2.5 md:px-5 md:mx-0 text-amber-200 border-amber-200 hover:bg-amber-200 hover:bg-opacity-10 bg-transparent focus:outline-none w-fit">
   <svg
