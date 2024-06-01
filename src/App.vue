ecc0662815849e8347d3edcda076153b5a986529
FrankIncognito <nolascofranklin09@gmail.com>
2024-06-01 12:09:22 +0800

    revert

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
     <nav class="max-w-7xl px-5 md:fixed top-0 z-[98] w-screen backdrop-blur-md bg-[#121212] bg-opacity-80">
       <div class="container mx-auto flex flex-wrap items-center justify-between">
         <button @click="redirectToHome" class="flex">
-          <span class="self-center text-lg text-[#6237A0] font-semibold whitespace-nowrap fadein-bot hover:text-amber-100">Franklin Nolasco</span>
+          <span class="self-center text-lg text-[#ffdb70] font-semibold whitespace-nowrap fadein-bot hover:text-amber-100">Franklin Nolasco</span>
         </button>
         <div class="flex md:order-2 fadein-bot">
           <a href="https://github.com/franklinnolasco7"><img class="w-9 rounded-full" src="https://seeklogo.com/images/G/github-logo-2E3852456C-seeklogo.com.png" alt="github"></a>
@@ -79,7 +79,7 @@ export default {
 }
 
 ::-webkit-scrollbar-thumb {
-  background: #6237A0;
+  background: #ffdb70;
   border-radius: 5px;
 }
 
