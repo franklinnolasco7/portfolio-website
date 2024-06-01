fec68580bcf2e95dbef88480ba47a338d637efa4
FrankIncognito <nolascofranklin09@gmail.com>
2024-06-01 20:59:20 +0800

    change

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,7 +11,7 @@
           <span class="wrap">{{ txt }}</span>
         </h1>
       </div>
-      <p class="text-white pr-4 fade-in-from-left">Welcome to My portfolio website. <span class="wave">👋</span></p>
+      <p class="text-white pr-4 fade-in-from-left">Welcome to My personal website. <span class="wave">👋</span></p>
       <br>
 <a href="https://drive.google.com/file/d/1m97ohzz4WbP_ibTxgQihO0cG2--bTv5i/view?usp=sharing"
   class="fadein-bot fade-500 flex items-center py-2 px-4 mx-auto text-sm font-medium rounded-lg border transition duration-300 md:py-2.5 md:px-5 md:mx-0 text-amber-200 border-amber-200 hover:bg-amber-200 hover:bg-opacity-10 bg-transparent focus:outline-none w-fit"
